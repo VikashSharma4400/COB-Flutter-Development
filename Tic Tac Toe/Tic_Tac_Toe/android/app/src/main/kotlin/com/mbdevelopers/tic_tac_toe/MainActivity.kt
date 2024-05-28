16c910b904b50e57a156d84a4a69ed14025bc7d9
Vikash Sharma <154815883+VikashSharma4400@users.noreply.github.com>
2024-05-28 10:32:20 +0530

    project added
    project added

---
+++ Tic Tac Toe/Tic_Tac_Toe/android/app/src/main/kotlin/com/mbdevelopers/tic_tac_toe/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mbdevelopers.tic_tac_toe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Tic Tac Toe/Tic_Tac_Toe/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:tic_tac_toe/enterPlayer.dart';
+
+
+void main() {
+
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(const MyApp());
+  });
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tic Tac Toe',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const enterPlayer(),
+    );
+  }
+}
